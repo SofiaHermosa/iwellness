b6bf4c410d31859c1fff397f5981a0d4b93e4a20
sophie <hermosasofia1118@gmail.com>
2021-11-21 01:09:17 +0800

    ##Changes## added front end enable ordering products for guest

--- resources/views/guest/sections/join-us.blade.php
+++ resources/views/guest/sections/join-us.blade.php
@@ -1,6 +1,6 @@
 <div class="block">
     <div data-aos="fade-right" data-aos-duration="1000" class="grid grid-cols-2 gap-0">
-        <div class="bg-gradient-to-br from-yellow-100 via-yellow-200 to-yellow-300 col-span-2 md:col-span-1">
+        <div class="bg-gradient-to-br from-yellow-100 via-yellow-200 to-yellow-300 col-span-2 md:col-span-1 py-20 md:py-0">
             <div class="text-center relative top-1/2 transform -translate-y-1/2 m-auto mt-4">
                 <h1 class="text-gray-900 font-extrabold text-xl md:text-3xl mb-4">Be part of us</h1>
                 <p class="text-gray-900 font-semibold text-sm md:text-md">With starting capital minimun of 700<br>be part of our community and acquire tons of perks</p>
