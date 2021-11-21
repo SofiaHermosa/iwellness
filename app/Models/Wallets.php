8ea86a18fa1f673a34bc7a9df70ac52ddba9d48f
sophie <hermosasofia1118@gmail.com>
2021-11-21 23:35:52 +0800

    ##Changes## disabled activity log in server for now

--- app/Models/Wallets.php
+++ app/Models/Wallets.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Spatie\Activitylog\Traits\LogsActivity;
+// use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Support\Facades\Crypt;
 
 class Wallets extends Model
