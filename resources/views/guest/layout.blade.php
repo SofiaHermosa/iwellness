433c171c1b3ca50decd5638a2b83cab5f7785712
sophie <hermosasofia1118@gmail.com>
2021-11-25 12:20:06 +0800

    ##Changes## revised referral link

--- resources/views/guest/layout.blade.php
+++ resources/views/guest/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Experience the difference">
+    <meta name="og:image" content="{{asset('assets/images/iwellness_logo_text.png')}}">
 
     <link rel="apple-touch-icon" href="{{asset('app/classic/base/assets/images/apple-touch-icon.png')}}">
     <link rel="shortcut icon" href="{{asset('assets/images/iwellness_logo.png')}}">
