1f6cc29c1129d1635062bec28de7307422e5d647
sophie <hermosasofia1118@gmail.com>
2021-11-27 16:37:26 +0800

    ##Changes## fixed the bug when the user request for cashout to dedcut it on current balance

--- app/Models/Diamonds.php
+++ app/Models/Diamonds.php
@@ -29,8 +29,13 @@ class Diamonds extends Model
     {
         $data   = json_decode($activity->properties)->attributes;
         $event  = $eventName == 'created' ? 'earned' : $eventName;
-
-        $description = ucfirst($event)." diamond total of ".number_format($data->amount, 2, '.', ',').' from '.config('constants.payment_transaction_type.'.$data->from);
+        $from   = $activity->causer_id == $data->user_id ? config('constants.payment_transaction_type.'.$data->from) : ($data->from == 1 ? 'referral bonus' : 'commission');
+        $member = $activity->causer_id == $data->user_id ? '' : ' from '.$activity->causer->username;     
+        $description = ucfirst($event)." ". $from ." total of ".number_format($data->amount, 2, '.', ',').' diamond '.$member;
+        if($activity->causer_id == $data->user_id){
+            $description = ucfirst($event)." diamond total of ".number_format($data->amount, 2, '.', ',').' from '.$from;
+        }
+        $activity->causer_id = $data->user_id;
         $activity->description = $description;
     }
 }
--- app/Models/Earnings.php
+++ app/Models/Earnings.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\Contracts\Activity;
 
 class Earnings extends Model
 {
@@ -23,4 +24,16 @@ class Earnings extends Model
     ];
 
     protected static $logFillable = true;
+
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+        $data   = json_decode($activity->properties)->attributes;
+        $event  = $eventName == 'created' ? 'earned' : $eventName;
+        $from   = $activity->causer_id == $data->user_id ? config('constants.payment_transaction_type.'.$data->from) : $data->from == 1 ? 'referral bonus' : 'commission';
+        $member = $activity->causer_id == $data->user_id ? '' : ' from '.$activity->causer->username;
+        $description = ucfirst($event)." ".$from ." total of ".number_format($data->amount, 2, '.', ',').$member;
+        
+        $activity->causer_id = $data->user_id;
+        $activity->description = $description;
+    }
 }
